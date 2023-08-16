2d4477df3f809fafbc62812e24248ed3c2ebcfc5
Polinakovr <polinakovrigina129@gmail.com>
2023-08-16 17:28:22 +0700

    TEST FUNCTIONS

---
+++ src/libhangman/checks.h
@@ -0,0 +1,7 @@
+#define MAX_LENGTH 20
+#define MAX_WORDS 10
+#define MAX_MISSES 6
+int checkLength(const char* str1);
+int isWord(const char* str1);
+int checkMaxWords(int num);
+int checkmaxmisses(int num);
